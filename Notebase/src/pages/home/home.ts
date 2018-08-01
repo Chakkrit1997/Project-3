527896accc55b041cbe3fb6b1f6c994356f5f5a2
Chakkrit1997 <guitar_dark_soft@hotmail.com>
2018-08-01 22:09:04 +0700

    Add Note

--- Notebase/src/pages/home/home.ts
+++ Notebase/src/pages/home/home.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+ 
+  notes = [];
+ 
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController) {
   }
+ 
   openCreateNewNote(){
-    
+    let modal = this.modalCtrl.create('CreateNewNotePage');
+    modal.present();
+ 
+    modal.onDidDismiss(data => {
+      if(!data.isCancel) {
+        this.notes.push(data.data);
+      } 
+    });
   }
-
+ 
 }
