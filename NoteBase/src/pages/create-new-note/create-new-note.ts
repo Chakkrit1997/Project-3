265b3f3d243f539966cab9cc7033af7a3b8ed18a
Chakkrit1997 <guitar_dark_soft@hotmail.com>
2018-08-01 23:56:20 +0700

    NewNote

---
+++ NoteBase/src/pages/create-new-note/create-new-note.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the CreateNewNotePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-create-new-note',
+  templateUrl: 'create-new-note.html',
+})
+export class CreateNewNotePage {
+ 
+  newNote = { title: '', message: '' }
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController) {
+  }
+ 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CreateNewNotePage');
+  }
+ 
+  save(){
+    
+  }
+ 
+  cancel(){
+ 
+  }
+ 
+}
